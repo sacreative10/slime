2fa7034d6f3e516a54b06ebf75710950b61bc850
Sohaib Alam <sacreative10@gmail.com>
2024-04-16 20:52:41 +0100

    Init

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,35 @@
+#include <CashewLib/Application.h>
+#include <CashewLib/EntryPoint.h>
 
-#include <cstdio>
+#include <CashewLib/Image.h>
+#include <iomanip>
 
-int main() { printf("Hello World\n"); }
+#include <CashewLib/Input/Input.h>
+#include <CashewLib/Input/KeyCodes.h>
+
+class ExampleLayer : public Cashew::Layer {
+public:
+  virtual void onUIRender() override {
+    ImGui::Begin("Example Layer");
+    ImGui::SetWindowSize(ImVec2(720, 1040));
+    ImGui::End();
+  }
+  // add input handling
+  virtual void onUpdate(float dt) override {
+    if (Cashew::Input::isKeyDown(Cashew::KeyCode::Tab)){}
+    else if (Cashew::Input::isKeyDown(Cashew::KeyCode::Escape)) {
+      Cashew::Application::Get().Close();
+    }
+  }
+};
+
+Cashew::Application *Cashew::CreateApplication(int argc, char **argv) {
+  Cashew::ApplicationSpecification spec;
+  spec.Name = "Cashew Example";
+  spec.Width = 720;
+  spec.Height = 1040;
+
+  Cashew::Application *app = new Cashew::Application(spec);
+  app->PushLayer<ExampleLayer>();
+  return app;
+}
